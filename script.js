8a9718a4d0167ad06542719be5fa43a627449cef
Gagan4399 <explorex6398@gmail.com>
2025-06-15 20:52:26 +0530

    Add files via upload
    These are my Portfolio Website Codes. This is my First Project.

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ========== Typing Animation =========== */
+var typed = new Typed('.typing', {
+    strings:["","Web Designer", "web Developer", "Editor", "YouTuber", "App Developer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true 
+})
